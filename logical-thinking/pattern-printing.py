43d237b0a53cca9c326c7650bca6389aa178351a
Saksham A Pathak <sakshamofficial0609@gmail.com>
2026-07-15 17:57:14 +0530

    Right-Half Pyramid Pattern

--- logical-thinking/pattern-printing.py
+++ logical-thinking/pattern-printing.py
@@ -1,10 +1,14 @@
 # Star Square of n*n
-
 def squarePattern(num):
     for i in range(num):
         for j in range(num):
             print("* ",end=" ")
         print()
 
-num = int(input())
-squarePattern(num)
+# Triangle Star Pattern
+def trianglePattern(num):
+    for i in range(num):
+        for j in range(i+1):
+            print("* ",end=" ")
+        print()
+
